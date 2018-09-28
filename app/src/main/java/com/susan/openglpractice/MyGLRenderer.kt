0a432e82379751a1c6061bf165c3f316ee165fc9
Sujin Kim <gfsusan@naver.com>
2018-09-28 20:58:43 +0900

    Rotate a shape

--- app/src/main/java/com/susan/openglpractice/MyGLRenderer.kt
+++ app/src/main/java/com/susan/openglpractice/MyGLRenderer.kt
@@ -3,6 +3,7 @@ package com.susan.openglpractice
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.opengl.Matrix
+import android.os.SystemClock
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -21,8 +22,13 @@ class MyGLRenderer : GLSurfaceView.Renderer {
         mSquare = Square()
     }
 
-    override fun onDrawFrame(unused: GL10) {
+    private val mRotationMatrix = FloatArray(16)
+
+    override fun onDrawFrame(gl: GL10) {
+        val scratch = FloatArray(16)
+
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+
 
         // Set the camera position (View matrix)
         Matrix.setLookAtM(mViewMatrix, 0, 0f, 0f, -3f, 0f, 0f, 0f, 0f, 1.0f, 0.0f)
@@ -30,10 +36,24 @@ class MyGLRenderer : GLSurfaceView.Renderer {
         // Calculate the projection and view transformation
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0)
 
+        /*
         // Draw shape
         mTriangle.draw(mMVPMatrix)
-//        mTriangle.draw()
+        // mTriangle.draw()
+*/
 
+        // Create a rotation transformation for the triangle
+        val time = SystemClock.uptimeMillis() % 4000L
+        val angle = 0.090f * time.toInt()
+        Matrix.setRotateM(mRotationMatrix, 0, angle, 0f, 0f, -1.0f)
+
+        // Combine the rotation matrix with the projection and camera view
+        // Note that the mMVPMatrix factor *must be first* in order
+        // for the matrix multiplication product to be correct.
+        Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0)
+
+        // Draw triangle
+        mTriangle.draw(scratch)
     }
 
     // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
