f7cacc7031333a4d3ae692fed20faf0f296af7fc
Sujin Kim <gfsusan@naver.com>
2018-09-28 17:34:28 +0900

    Define a projection

--- app/src/main/java/com/susan/openglpractice/MyGLRenderer.kt
+++ app/src/main/java/com/susan/openglpractice/MyGLRenderer.kt
@@ -2,6 +2,7 @@ package com.susan.openglpractice
 
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
+import android.opengl.Matrix
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -26,13 +27,19 @@ class MyGLRenderer : GLSurfaceView.Renderer {
         mTriangle.draw()
     }
 
-
+    // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+    private val mMVPMatrix = FloatArray(16)
+    private val mProjectionMatrix = FloatArray(16)
+    private val mViewMatrix = FloatArray(16)
 
     override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
         GLES20.glViewport(0, 0, width, height)
 
-        // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+        val ratio: Float = width.toFloat() / height.toFloat()
 
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f, 7f)
     }
 
 
